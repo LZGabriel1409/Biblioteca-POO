885d58fa60ef513eaa3573692d3675471210290b
wagnermarques <wagnerdocri@gmail.com>
2026-02-24 11:08:26 -0300

    classe usuario

---
+++ src/Usuario.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+
+public class Usuario {
+    String nome = JOptionPane.showInputDialog("Escreva o nome do usuário:");
+    String cpf = JOptionPane.showInputDialog("Escreva o CPF do usuário:");
+}
