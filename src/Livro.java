a96c6492ae43f6c9c2d8dfd31c55ddb159d38211
LZGabriel1409 <lg.moraes.lima@gmail.com>
2026-03-05 19:39:42 -0300

    biblioteca

--- src/Livro.java
+++ src/Livro.java
@@ -1,21 +1,38 @@
 public class Livro {
-    private String codigo;
+
     private String titulo;
     private String autor;
+    private String codigo;
+    private boolean emprestado;
 
     public Livro(String titulo, String autor, String codigo){
         this.titulo = titulo;
         this.autor = autor;
         this.codigo = codigo;
+        this.emprestado = false;
     }
 
     public String getTitulo(){
         return titulo;
     }
-    public String getAutor(){
-        return autor;
-    }
+
     public String getCodigo(){
         return codigo;
+    }
+
+    public boolean isEmprestado(){
+        return emprestado;
+    }
+
+    public void emprestar(){
+        emprestado = true;
+    }
+
+    public void devolver(){
+        emprestado = false;
+    }
+
+    public String toString(){
+        return "Titulo: " + titulo + " | Autor: " + autor;
     }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,27 +1,34 @@
 public class Usuario {
-    private String codigo;
+
     private String nome;
     private String cpf;
     private String numero;
+    private String codigo;
 
-    public Usuario (String nome, String cpf, String numero, String codigo) {
-        this.cpf = cpf;
+    public Usuario(String nome, String cpf, String numero, String codigo){
         this.nome = nome;
+        this.cpf = cpf;
         this.numero = numero;
+        this.codigo = codigo;
     }
 
     public String getNome(){
         return nome;
     }
-    public String getCPF(){
+
+    public String getCpf(){
         return cpf;
     }
+
     public String getNumero(){
         return numero;
     }
+
     public String getCodigo(){
         return codigo;
     }
 
-
+    public String toString(){
+        return "Nome: " + nome + " | CPF: " + cpf;
+    }
 }
